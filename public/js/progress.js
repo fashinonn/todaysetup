808bab49aa9117b2889d739bdde8758a78b2a9cd
fashinonn <werik4162@gmail.com>
2025-12-18 00:27:22 +0900

    Delete public directory

--- public/js/progress.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-        var a = 0,
-          b = setInterval(function() {
-            a += 10;
-            $("#dynamic").css("width", a + "%").attr("aria-valuenow", a).text(a + "% 完了");
-            100 <= a && clearInterval(b)
-          }, 100)
-      });
